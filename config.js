68d207eb2acc438107bc850d2e8beae99eee64e4
LORD PREMO <cypherx599@gmail.com>
2026-02-28 20:00:23 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  botName: "BROKEN LORD MD",
+  ownerName: "BROKENLORD",
+  ownerNumber: "255773001107",
+  menuImage: "https://upcdn.io/kW2K8mM/raw/uploads/2026/02/17/4j9r78e4Jt-image.jpg%20(20).png",
+  port: process.env.PORT || 3000,
+  prefix: ".",
+  globalSettings: {
+    alwaysOnline: true,
+    autoTyping: true,
+    autoRecording: false,
+    autoReact: true,
+    autoViewStatus: true,
+    autoRead: true
+  }
+};
